5a0c42ed9af87cc28188649ce872f1b051fa7a75
James Nutt <james.srn@gmail.com>
2013-12-02 13:19:03 +0000

    main file

---
+++ Flowers.py
@@ -0,0 +1,8 @@
+print ".--------------------------------------------------------."
+print "| Welcome to Flowers. A text adventure by ORG_NAME_HERE. |"
+print "|         ToxicVenom         dot|not        jsrn         |"
+print "`--------------------------------------------------------'"
+
+# set up world/player objects
+
+# enter game loop
