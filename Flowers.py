372b7413cc9fa5dba26e897cc100b9385f5c4ee5
James Nutt <james.srn@gmail.com>
2013-12-02 18:57:20 +0000

    rejiggered banner

--- Flowers.py
+++ Flowers.py
@@ -1,12 +1,17 @@
-print ".--------------------------------------------------------."
-print "| Welcome to Flowers. A text adventure by ORG_NAME_HERE. |"
-print "|         ToxicVenom         dot|not        jsrn         |"
-print "`--------------------------------------------------------'"
+def print_banner():
+	print ".----------------------------- FLOWERS -----------------------------."
+	print "|                   A(n) ORG_NAME_HERE Adventure                    |"
+	print "|          dot|not           ToxicVenom             jsrn            |"
+	print "`-------------------------------------------------------------------'"
+
+def handle_input(input):
+	print input
 
 # set up world/player objects
 
-# enter game loop
+print_banner()
 
+# enter game loop
 while True:
 	input = raw_input("> ")
-	print input
+	handle_input(input)
