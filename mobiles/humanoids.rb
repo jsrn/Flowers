a865b884f68588fee7eb805f742c75d782d93f33
James Nutt <james.srn@gmail.com>
2013-12-03 17:47:48 +0000

    super last, set stats

--- mobiles/humanoids.rb
+++ mobiles/humanoids.rb
@@ -2,8 +2,9 @@ require_relative "mobile.rb"
 
 class Orc < Mobile
 	def initialize
-		super
 		@name = "an orc"
 		@description = "A horrible orc!"
+		@str, @dex, @int = 10, 5, 1
+		super
 	end
 end
