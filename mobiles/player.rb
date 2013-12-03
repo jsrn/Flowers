5c260f401369c1c3346ebc9c3bc11c37bba9ce70
James Nutt <james.srn@gmail.com>
2013-12-03 15:35:53 +0000

    status print method

--- mobiles/player.rb
+++ mobiles/player.rb
@@ -4,7 +4,13 @@ class Player < Mobile
 
 	def pick_name
 		puts "Greetings, traveller! What is your name?"
+		print "> "
 		@name = gets.chomp
 		puts "Greetings, " + @name
+	end
+
+	def print_status
+		puts "Name: #{@name}"
+		puts "HP: #{@current_hp}/#{max_hp}"
 	end
 end
