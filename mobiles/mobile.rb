695f8d0ac6038a34fa0b9d03decbaf552665b966
James Nutt <james.srn@gmail.com>
2013-12-03 15:35:40 +0000

    accessors for stats n stuff

--- mobiles/mobile.rb
+++ mobiles/mobile.rb
@@ -1,5 +1,26 @@
 class Mobile
 
+	attr_reader :level
+
+	attr_reader :current_hp
+	attr_reader	:str
+	attr_reader :dex
+	attr_reader :int
+
 	attr_reader :name
-	
+
+	def initialize
+		@current_hp = max_hp
+	end
+
+	def set_stats(str,dex,int)
+		@str = str
+		@dex = dex
+		@int = int
+	end
+
+	def max_hp
+		77
+	end
+
 end
