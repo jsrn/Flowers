f76cc9ff1decc5e2bf2c4031298f17734b0df731
James Nutt <james.srn@gmail.com>
2013-12-05 16:03:10 +0000

    direction checking

--- world/world.rb
+++ world/world.rb
@@ -5,7 +5,7 @@ class World
 		@x, @y = 0, 0
 		@rooms = {}
 		@current_room = Room.new
-		@current_room.exits = "nesw"
+		@current_room.exits = "north|east|south|west"
 		store_room( @current_room, @x, @y)
 	end
 
@@ -16,5 +16,13 @@ class World
 	def store_room( room, x, y )
 		@rooms[x] = {} if @rooms[x].nil?
 		@rooms[x][y] = room
+	end
+
+	def try_movement(direction)
+		if @current_room.has_exit?(direction)
+			puts "Trying to go #{direction}"
+		else
+			puts "You cannot go that way."
+		end
 	end
 end
