3f222d2cfc44ae1adc31aa42cd82b3f4e61069f6
James Nutt <james.srn@gmail.com>
2013-12-05 14:13:52 +0000

    list exits

--- world/room.rb
+++ world/room.rb
@@ -1,10 +1,37 @@
 class Room
+
+	attr_accessor :exits
+
 	def initialize
 		@description = get_random_blurb
 	end
 
 	def describe
 		puts @description
+
+		print "There are exits to the "
+		puts exit_long_form
+	end
+
+	def exit_long_form
+		exit_names = {
+			:n => "north",
+			:e => "east",
+			:s => "south",
+			:w => "west"
+		}
+
+		exit_letters = @exits.split("")
+
+		exit_letters.map! do |letter|
+			exit_names[letter.to_sym]
+		end
+
+		if exit_letters.length > 1
+			exit_letters[-1] = "and " << exit_letters[-1]
+		end
+
+		exit_letters.join(", ")
 	end
 
 	def get_random_blurb
