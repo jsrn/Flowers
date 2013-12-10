2895934306bb30edb612c7bab4d44a3593c2ca60
James Nutt <james.srn@gmail.com>
2013-12-10 13:46:54 +0000

    reordered init, orc face

--- mobiles/humanoids.rb
+++ mobiles/humanoids.rb
@@ -20,18 +20,29 @@ end
 
 class Orc < Humanoid
 	def initialize
+		super
 		@name = "an orc"
 		@description = "A horrible orc!"
 		@str, @dex, @int = 10, 5, 1
-		super
+		@profile_picture = [
+			"  ______  ",
+			" /      \\ ",
+			"| @    @ |",
+			"|        |",
+			"|   ^^   |",
+			"| ______ |",
+			"| VV  VV |",
+			" \\______/ "
+		]
+		@hp = max_hp
 	end
 end
 
 class Drow < Humanoid
 	def initialize
+		super
 		@name = "a drow"
 		@description = "A dark elf, fast, cunning and cruel."
 		@str, @dex, @int = 3, 5, 5
-		super
 	end
 end
