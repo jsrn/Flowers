05078e765a78af3f2b8925f7e159c2fb8241ec31
James Nutt <james.srn@gmail.com>
2013-12-10 14:08:52 +0000

    generate initial monsters

--- world/room.rb
+++ world/room.rb
@@ -1,13 +1,28 @@
 class Room
 	attr_accessor :exits
+	attr_accessor :monsters
 
 	def initialize
 		@description = get_random_blurb
+		@monsters = generate_monsters
 	end
 
 	def describe
 		puts @description
 		puts "There are exits to the #{exit_long_form}."
+		puts "Lurking in the area you see #{monsters_long_form}."
+	end
+
+	def generate_monsters
+		monsters = []
+		monsters << Orc.new
+		monsters << Orc.new
+		monsters << Drow.new
+	end
+
+	def monsters_long_form
+		names = @monsters.collect { |x| x.name }
+		names.join(", ")
 	end
 
 	def exit_long_form
