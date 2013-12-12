e1affc88b3a42a73af0e4cf81eb8bd2764355f12
James Nutt <james.srn@gmail.com>
2013-12-12 14:06:59 +0000

    fixed indent

--- mobiles/player.rb
+++ mobiles/player.rb
@@ -2,39 +2,37 @@ require_relative "mobile.rb"
 
 class Player < Mobile
 
-	def initialize
-		super
-		@name = "Douglas"
-		@description = "The intrepid hero."
-		@str, @dex, @int = 1, 1, 1
-		@profile_picture = [
-			"   @@     ",
-			"  .@@. |D ",
-			"  @@@@ |  ",
-			"  @@@ @   ",
-			"  .@@.    ",
-			"  @  @    ",
-			"  @  @    ",
-			" ==  ==   "
-		]
-		@hp = max_hp
-		@xp = 0
-	end
+  def initialize
+    super
+    @name = "Douglas"
+    @description = "The intrepid hero."
+    @str, @dex, @int = 1, 1, 1
+    @profile_picture = [
+      "   @@     ",
+      "  .@@. |D ",
+      "  @@@@ |  ",
+      "  @@@ @   ",
+      "  .@@.    ",
+      "  @  @    ",
+      "  @  @    ",
+      " ==  ==   "
+    ]
+    @hp = max_hp
+    @xp = 0
+  end
 
-	def print_status
-		describe
-		puts "| Inventory:"
-	end
-    def grant_xp(xp)
-        @xp += xp
-    end
+  def print_status
+    describe
+    puts "| Inventory:"
+  end
 
-    def can_level_up?
-    end
-    def level_up
+  def grant_xp(xp)
+    @xp += xp
+  end
 
-        
+  def can_level_up?
+  end
 
-
-	end
+  def level_up
+  end
 end
