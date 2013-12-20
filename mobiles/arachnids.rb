4c2e798b016247a82fe58c8a623a0b1565d9bad0
James Nutt <james.srn@gmail.com>
2013-12-20 11:22:15 +0000

    arachnids

---
+++ mobiles/arachnids.rb
@@ -0,0 +1,11 @@
+require_relative "mobile.rb"
+
+class Spider < Mobile
+  def initialize
+    super
+    @name = "a spider"
+    @description = "A spider hunting for flesh."
+    @str, @dex, @int = 3, 5, 3
+    @hp = max_hp
+  end
+end
