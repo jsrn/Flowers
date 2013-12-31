e2a58baeb7a93683cde1f229cd2e0299b3c5f48d
James Nutt <james.srn@gmail.com>
2013-12-31 19:59:09 +0000

    spider

--- mobiles/arachnids.rb
+++ mobiles/arachnids.rb
@@ -6,6 +6,16 @@ class Spider < Mobile
     @name = "a spider"
     @description = "A spider hunting for flesh."
     @str, @dex, @int = 3, 5, 3
+    @profile_picture = [
+      '          ',
+      '  ______  ',
+      ' /oo oo \ ',
+      ' \ vvv  / ',
+      '  \----/  ',
+      '  /||||\  ',
+      '          ',
+      '          '
+    ]
     @hp = max_hp
   end
 end
